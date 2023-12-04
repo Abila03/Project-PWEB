cee71222b712c9a8b29ea407066203ed86e6b58d
Abila <100850901+Abila03@users.noreply.github.com>
2023-12-05 01:24:04 +0700

    Add files via upload

---
+++ application/views/dashboard.php
@@ -0,0 +1,224 @@
+<!-- Page Content -->
+<!-- Banner Starts Here -->
+<div class="main-banner header-text">
+  <div class="container-fluid">
+    <div class="owl-banner owl-carousel">
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/product-7-720x480.jpg" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <div class="meta-category">
+            </div>
+            <a href="fleet.html"><h4>Hyundai Ioniq</h4></a>
+            <ul class="post-info">
+              <li>Mobil Listrik idaman keluarga zaman NOW</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/product-9-720x480.jpg" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <div class="meta-category">
+            </div>
+            <a href="fleet.html"><h4>Mazda 5</h4></a>
+            <ul class="post-info">
+              <li>Mobil Harian Idaman Karyawan</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/product-3-720x480.jpg" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <div class="meta-category">
+            </div>
+            <a href="fleet.html"><h4>Opel</h4></a>
+            <ul class="post-info">
+              <li>Mobil Keluarga Besar untuk Keliling Unej</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/product-4-720x480.jpg" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <div class="meta-category">
+            </div>
+            <a href="fleet.html"><h4>Ferrari</h4></a>
+            <ul class="post-info">
+              <li>Pemikat perhatian Pujaan Hati</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/product-5-720x480.jpg" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <div class="meta-category">
+            </div>
+            <a href="fleet.html"><h4>Buggati</h4></a>
+            <ul class="post-info">
+              <li>Mobil nyaman Anak Kuliahan</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/product-6-720x480.jpg" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <div class="meta-category">
+              <span>
+            </div>
+            <a href="fleet.html"><h4>Jaguar</h4></a>
+            <ul class="post-info">
+              <li>Mobil Mewah Aura Memancar</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Banner Ends Here -->
+
+<section class="blog-posts grid-system">
+  <div class="container">
+    <div class="all-blog-posts">
+      <h2 class="text-center">Our Teams</h2>
+      <br>
+      <div class="row">
+        <div class="col-md-4 col-sm-6">
+          <div class="blog-post">
+            <div class="blog-thumb">
+              <img src="<?= base_url('assets/assets_shop') ?>/images/team-image-1-646x680.jpg" alt="">
+            </div>
+            <div class="down-content">
+              <strong>222410102083</strong>
+              <a><h4>Muhamad Abila Nurrodin</h4></a>
+              <div class="post-options">
+                <div class="row">
+                  <div class="col-lg-12">
+                    <ul class="post-tags"></ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+          <div class="blog-post">
+            <div class="blog-thumb">
+              <img src="<?= base_url('assets/assets_shop') ?>/images/team-image-4-646x680.jpg" alt="">
+            </div>
+            <div class="down-content">
+              <strong></strong>
+              <a><h4>Rheno Julius Andreas</h4></a>
+              <div class="post-options">
+                <div class="row">
+                  <div class="col-lg-12">
+                    <ul class="post-tags">
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+          <div class="blog-post">
+            <div class="blog-thumb">
+              <img src="<?= base_url('assets/assets_shop') ?>/images/team-image-3-646x680.jpg" alt="">
+            </div>
+            <div class="down-content">
+              <strong>from</strong> <span> $120</span> <strong>per weekend</strong>
+              <a href="offers.html"><h4>Septi Wulandari </h4></a>
+              <div class="post-options">
+                <div class="row">
+                  <div class="col-lg-12">
+                    <ul class="post-tags">
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="blog-posts grid-system">
+  <div class="container">
+    <div class="all-blog-posts">
+      <h2 class="text-center">Blog</h2>
+      <br>
+      <div class="row">
+        <div class="col-md-4 col-sm-6">
+          <div class="blog-post">
+            <div class="blog-thumb">
+              <img src="<?= base_url('assets/assets_shop') ?>/images/blog-1-720x480.jpg" alt="">
+            </div>
+            <div class="down-content">
+              <a href="blog-details.html"><h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h4></a>
+              
+              <p>Nullam nibh mi, tincidunt sed sapien ut, rutrum hendrerit velit. Integer auctor a mauris sit amet eleifend.</p>
+
+              <ul class="post-info">
+                <li><a href="#">John Doe</a></li>
+                <li><a href="#">10.07.2020 10:20</a></li>
+                <li><a href="#"><i class="fa fa-comments" title="Comments"></i> 12</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+          <div class="blog-post">
+            <div class="blog-thumb">
+              <img src="<?= base_url('assets/assets_shop') ?>/images/blog-2-720x480.jpg" alt="">
+            </div>
+            <div class="down-content">
+              <a href="blog-details.html"><h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h4></a>
+              
+              <p>Nullam nibh mi, tincidunt sed sapien ut, rutrum hendrerit velit. Integer auctor a mauris sit amet eleifend.</p>
+
+              <ul class="post-info">
+                <li><a href="#">John Doe</a></li>
+                <li><a href="#">10.07.2020 10:20</a></li>
+                <li><a href="#"><i class="fa fa-comments" title="Comments"></i> 12</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+          <div class="blog-post">
+            <div class="blog-thumb">
+              <img src="<?= base_url('assets/assets_shop') ?>/images/blog-3-720x480.jpg" alt="">
+            </div>
+            <div class="down-content">
+              <a href="blog-details.html"><h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h4></a>
+              
+              <p>Nullam nibh mi, tincidunt sed sapien ut, rutrum hendrerit velit. Integer auctor a mauris sit amet eleifend.</p>
+
+              <ul class="post-info">
+                <li><a href="#">John Doe</a></li>
+                <li><a href="#">10.07.2020 10:20</a></li>
+                <li><a href="#"><i class="fa fa-comments" title="Comments"></i> 12</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+
+<div style="height: 180px;"></div>
